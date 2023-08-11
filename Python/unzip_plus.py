425b0ae6b854cadaa642505450794f1af0bdfd33
delale <alessandro.deluca@uzh.ch>
2023-08-11 09:30:05 +0200

    Added logging

--- Python/unzip_plus.py
+++ Python/unzip_plus.py
@@ -5,6 +5,7 @@ Code created aided by GPT-3.
 """
 
 import argparse
+import logging
 import os
 import zipfile
 import shutil
@@ -18,9 +19,6 @@ def extract_and_filter_zips(input_directory, output_directory, to_remove) -> Non
             f"{os.path.dirname(output_directory)} " +\
                 "does not exist. Nowhere to create output directory."
             )
-    
-    # Create the output directory if it doesn't exist
-    os.makedirs(output_directory, exist_ok=True)
 
     # Make to remove a list
     if type(to_remove) is str:
@@ -32,38 +30,73 @@ def extract_and_filter_zips(input_directory, output_directory, to_remove) -> Non
         for i in to_remove:
             if type(i) is not str:
                 raise ValueError("All items in to_remove should be strings.")
+    
+    # Create the output directory if it doesn't exist
+    os.makedirs(output_directory, exist_ok=True)
+
+    # Init logger
+    logging.basicConfig(
+        filename=os.path.join(output_directory, "unzip_plus.log"),
+        encoding='utf-8', level=logging.DEBUG
+    )
+    logging.info(
+        f"unzip_plus.py called from {os.getcwd()} with parameters:\n" +\
+        f"\t- input_directory={input_directory}\n" +\
+        f"\t- output_directory={output_directory}\n" +\
+        f"\t- remove={to_remove}\n" + "-"*15 + "\n\n"
+        )
+
     # Iterate through each file in the input directory
     for filename in os.listdir(input_directory):
         filepath = os.path.join(input_directory, filename)
 
         # Check if the file is a zip archive
         if filename.endswith(".zip"):
-            with zipfile.ZipFile(filepath, 'r') as zip_ref:
-                # Extract each item in the archive
-                for item in zip_ref.infolist():
-                    if os.path.dirname(item.filename).split('/')[-1] not in to_remove:
-                        # Extract the item if not in to_remove
-                        zip_ref.extract(item, output_directory)
 
-
-                    ## Deprecated code by GPT-3: does not work as intended
-                    # # Get the extracted item's path
-                    # extracted_path = os.path.join(output_directory, item.filename)
+            try:
+                with zipfile.ZipFile(filepath, 'r') as zip_ref:
+                    logging.INFO(f"Extracting {filename}...")
                     
-                    
-                    # # Remove the to_remove subfolder(s) if it exists
-                    # for subfolder in to_remove:
-                    #     remove_path = os.path.join(extracted_path, subfolder)
-                    #     if os.path.exists(remove_path) and os.path.isdir(remove_path):
-                    #         shutil.rmtree(remove_path)
+                    # Extract each item in the archive
+                    for item in zip_ref.infolist():
+                        if os.path.dirname(item.filename).split('/')[-1] not in to_remove:
+                            if not (
+                                os.path.isfile(os.path.join(output_directory, item.filename)) or\
+                                    os.path.isdir(os.path.join(output_directory, item.filename))
+                            ):
+                                # Extract the item if not in to_remove
+                                zip_ref.extract(item, output_directory)
+                                logging.INFO(
+                                    f"Extracted {item.filename} to " +\
+                                    f"{os.path.join(output_directory, item.filename)}"
+                                    )
+                            else:
+                                logging.INFO(
+                                    f"{os.path.join(output_directory, item.filename)} already exists."
+                                    )
 
-                    ## Only needed if you want to store each subfolder file outside of the subfolder.  
-                    # # Move the extracted content to the final destination
-                    # extracted_items = os.listdir(extracted_path)
-                    # for extracted_item in extracted_items:
-                    #     extracted_item_path = os.path.join(extracted_path, extracted_item)
-                    #     shutil.move(extracted_item_path, output_directory)
-                    # os.rmdir(extracted_path)
+                        ## Deprecated code by GPT-3: does not work as intended
+                        # # Get the extracted item's path
+                        # extracted_path = os.path.join(output_directory, item.filename)
+                        
+                        
+                        # # Remove the to_remove subfolder(s) if it exists
+                        # for subfolder in to_remove:
+                        #     remove_path = os.path.join(extracted_path, subfolder)
+                        #     if os.path.exists(remove_path) and os.path.isdir(remove_path):
+                        #         shutil.rmtree(remove_path)
+
+                        ## Only needed if you want to store each subfolder file outside of the subfolder.  
+                        # # Move the extracted content to the final destination
+                        # extracted_items = os.listdir(extracted_path)
+                        # for extracted_item in extracted_items:
+                        #     extracted_item_path = os.path.join(extracted_path, extracted_item)
+                        #     shutil.move(extracted_item_path, output_directory)
+                        # os.rmdir(extracted_path)
+                        
+            except zipfile.BadZipFile() as e:
+                logging.error(e + f"\nOn file {filepath}")
+        
 
 def test() -> None:
     # Example usage:
