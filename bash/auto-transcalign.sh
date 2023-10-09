9f298ca933471ed19d389f99a969530c6961b22f
delale@MacBookPro <alessandro.deluca@uzh.ch>
2023-10-09 13:57:15 +0200

    init bash scripts dir

---
+++ bash/auto-transcalign.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#####################################################################
+# Bash script to use automatically:                                 #
+#   1. resample audio to 16000 kHz                                  #
+#   2. transcribe audio files in a directory using whisper.cpp      #
+#   3. force-align using montreal-forced-aligner                    #
+#                                                                   #
+# Alessandro De Luca - 10.2023                                      #
+#####################################################################
+
